04493bba3bfc342ecee3dedad3011dc24a2ef96b
lependu <lependu@protonmail.com>
2018-08-21 19:26:02 +0200

    Refactors plugin test

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,18 +1,17 @@
 'use strict'
 
-const tap = require('tap')
+const { test } = require('tap')
 const Fastify = require('fastify')
 const plugin = require('../plugin')
 
-const { test } = tap
-
-test('decorates request with detectedLng', t => {
-  t.plan(1)
+test('Decorates request with detectedLng', t => {
+  t.plan(2)
   const fastify = Fastify()
 
   fastify
     .register(plugin, { fallbackLng: 'hu' })
-    .ready(() => {
+    .ready(err => {
+      t.error(err)
       t.equal(fastify._Request.prototype.detectedLng, 'hu',
         'default value is fallbackLng'
       )
@@ -20,51 +19,27 @@ test('decorates request with detectedLng', t => {
   fastify.close()
 })
 
-test('throws if order option is not an array', t => {
-  t.plan(1)
-  const fastify = Fastify()
+test('Register errors', t => {
+  t.plan(6)
 
-  fastify
-    .register(plugin, { order: 'not-array' })
-    .after(err => {
-      t.equal(err.message, 'options.order has to be an array',
-        'error message matches'
-      )
-    })
-  fastify.close()
-})
-
-test('throws if order option contains invalid parser', t => {
-  t.plan(1)
-  const fastify = Fastify()
-
-  fastify
-    .register(plugin, { order: ['not-exists'] })
-    .after(err => {
-      t.equal(err.message, 'not-exists is not a valid language parser',
-        'error message matches'
-      )
-    })
-  fastify.close()
-})
-
-test('throws if order option contains the same parser multiple times', t => {
-  t.plan(1)
-  const fastify = Fastify()
-
-  fastify
-    .register(plugin, { order: ['query', 'query'] })
-    .after(err => {
-      t.equal(err.message, 'query parser found  multiple times in order option. Try scope your routes instead.',
-        'error message matches'
-      )
-    })
-  fastify.close()
+  testRegisterError(t, { order: 'not-array' },
+    'options.order has to be an array',
+    'throws if order option is not an array'
+  )
+  testRegisterError(t, { order: ['not-exists'] },
+    'not-exists is not a valid language parser',
+    'throws if order option contains invalid parser'
+  )
+  testRegisterError(t, { order: ['query', 'query'] },
+    'query parser found  multiple times in order option.',
+    'throws if order option contains the same parser multiple times'
+  )
 })
 
 test('Integration', t => {
   t.plan(19)
   const fastify = Fastify()
+  t.tearDown(() => fastify.close.bind(fastify))
 
   fastify
     .register((scope1, opts, next) => {
@@ -110,7 +85,7 @@ test('Integration', t => {
         })
       next()
     })
-    .after(err => {
+    .ready(err => {
       t.error(err)
       t.equal(fastify._hooks.preHandler.length, 0,
         'Out of scope does not add preHandler hook'
@@ -118,59 +93,47 @@ test('Integration', t => {
       t.equal(typeof fastify._Request.prototype.detectedLng, 'undefined',
         'Out of scope does not decorates req with detectedLng property'
       )
+      testRoute(t, fastify, '/scope1', {}, 'en',
+        'GET /scope1 wo header and query returns defaultLng'
+      )
+      testRoute(t, fastify, '/scope1',
+        { 'accept-language': 'hu;q=0.9,de;q=0.8' },
+        'de',
+        'GET /scope1 with header only picks from header'
+      )
+      testRoute(t, fastify, '/scope1?lng=de',
+        { 'accept-language': 'en;q=0.8' },
+        'de',
+        'GET /scope1 with query and header returns query value'
+      )
+      testRoute(t, fastify, '/scope2/wo-path-param', {}, 'fr',
+        'GET /scope2/wo-path-param returns session value'
+      )
+      testRoute(t, fastify, '/scope2/fr/with-path-param', {}, 'fr',
+        'GET /scope2/fr/with-path-param returns value found in path param'
+      )
     })
-
-  fastify.inject({
-    method: 'GET',
-    url: '/scope1'
-  }, (err, res) => {
-    t.error(err)
-    t.ok(~res.payload.indexOf('en'),
-      'GET /scope1 wo header and query returns defaultLng'
-    )
-  })
-
-  fastify.inject({
-    method: 'GET',
-    url: '/scope1',
-    headers: { 'accept-language': 'hu;q=0.9,de;q=0.8' }
-  }, (err, res) => {
-    t.error(err)
-    t.ok(~res.payload.indexOf('de'),
-      'GET /scope1 with header only picks from header'
-    )
-  })
-
-  fastify.inject({
-    method: 'GET',
-    url: '/scope1?lng=de',
-    headers: { 'accept-language': 'en;q=0.8' }
-  }, (err, res) => {
-    t.error(err)
-    t.ok(~res.payload.indexOf('de'),
-      'GET /scope1 with query and header returns query value'
-    )
-  })
-
-  fastify.inject({
-    method: 'GET',
-    url: '/scope2/wo-path-param'
-  }, (err, res) => {
-    t.error(err)
-    t.ok(~res.payload.indexOf('fr'),
-      'GET /scope2/wo-path-param returns session value'
-    )
-  })
-
-  fastify.inject({
-    method: 'GET',
-    url: '/scope2/fr/with-path-param'
-  }, (err, res) => {
-    t.error(err)
-    t.ok(~res.payload.indexOf('fr'),
-      'GET /scope2/fr/with-path-param returns value found in path param'
-    )
-  })
-
-  fastify.close()
 })
+
+function testRegisterError (t, opts, check, msg) {
+  const fastify = Fastify()
+  t.tearDown(() => fastify.close.bind(fastify))
+
+  fastify
+    .register(plugin, opts)
+    .ready(err => {
+      t.ok(err instanceof Error)
+      t.match(err.message, check, msg)
+    })
+}
+
+function testRoute (t, instance, url, headers, check, msg) {
+  instance.inject({
+    method: 'GET',
+    url,
+    headers
+  }, (err, res) => {
+    t.error(err)
+    t.ok(~res.payload.indexOf(check), msg)
+  })
+}
