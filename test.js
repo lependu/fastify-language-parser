8d4ecc7c0f5e4d242ac8883b032c72bbe5c9cb29
lependu <lependu@protonmail.com>
2018-08-22 20:37:42 +0200

    Refactors parser tests

--- test.js
+++ test.js
@@ -3,6 +3,8 @@
 const { test } = require('tap')
 const Fastify = require('fastify')
 const plugin = require('../plugin')
+const commonParser = require('../lib/common-parser')
+const headerParser = require('../lib/header-parser')
 
 test('Decorates request with detectedLng', t => {
   t.plan(2)
@@ -24,16 +26,107 @@ test('Register errors', t => {
 
   testRegisterError(t, { order: 'not-array' },
     'options.order has to be an array',
-    'throws if order option is not an array'
-  )
+    'throws if order option is not an array')
+
   testRegisterError(t, { order: ['not-exists'] },
     'not-exists is not a valid language parser',
-    'throws if order option contains invalid parser'
-  )
+    'throws if order option contains invalid parser')
+
   testRegisterError(t, { order: ['query', 'query'] },
     'query parser found  multiple times in order option.',
-    'throws if order option contains the same parser multiple times'
-  )
+    'throws if order option contains the same parser multiple times')
+})
+
+test('Common parser', t => {
+  t.plan(5)
+
+  testParser(t, commonParser({
+    decorator: 'foo', key: 'bar', supportedLngs: ['alpha', 'beta'] }),
+  { detectedLng: 'alpha', foo: { bar: 'beta' } },
+  'with check | check is true | with key',
+  'beta',
+  'sets req.detectedLng')
+
+  testParser(t, commonParser({
+    decorator: 'foo', key: 'bar', supportedLngs: ['alpha', 'beta'] }),
+  { detectedLng: 'alpha' },
+  'with check | check is true | without key',
+  'alpha',
+  'does not set req.detectedLng')
+
+  testParser(t, commonParser({
+    decorator: 'foo', key: 'bar', supportedLngs: ['alpha', 'beta'] }),
+  { detectedLng: 'alpha', foo: { bar: 'gamma' } },
+  'with check | check is false | with key',
+  'alpha',
+  'does not set req.detectedLng')
+
+  testParser(t, commonParser({
+    decorator: 'foo', key: 'bar', supportedLngs: [] }),
+  { detectedLng: 'alpha', foo: { bar: 'beta' } },
+  'without check | with key',
+  'beta',
+  'sets req.detectedLng')
+
+  testParser(t, commonParser({
+    decorator: 'foo', key: 'bar', supportedLngs: [] }),
+  { detectedLng: 'alpha' },
+  'without check | without key',
+  'alpha',
+  'does not set req.detectedLng')
+})
+
+test('Header parser', t => {
+  t.plan(5)
+
+  testParser(t, headerParser({
+    decorator: 'headers', key: 'accept-language', supportedLngs: ['hu', 'en']
+  }),
+  { detectedLng: 'en',
+    headers: { 'accept-language': 'en-GB;q=0.9,hu;q=0.7' } },
+  'with check | with header | with matches',
+  'hu',
+  'picks the first matching item from supportedLngs')
+
+  testParser(t, headerParser({
+    decorator: 'headers', key: 'accept-language', supportedLngs: ['hu', 'en']
+  }),
+  { detectedLng: 'en',
+    headers: { 'accept-language': 'de;q=0.9,fr;q=0.8' } },
+  'with check | with header | without matches',
+  'en',
+  'does not set req.detectedLng')
+
+  testParser(t, headerParser({
+    decorator: 'headers', key: 'accept-language', supportedLngs: ['hu', 'de']
+  }),
+  { detectedLng: 'en' },
+  'with check | without header',
+  'en',
+  'does not set req.detectedLngs')
+
+  testParser(t, headerParser({
+    decorator: 'headers', key: 'accept-language', supportedLngs: []
+  }),
+  { detectedLng: 'en' },
+  'without check | without header',
+  'en',
+  'does not set req.detectedLngs')
+
+  t.test('without check | with header', t => {
+    t.plan(1)
+    const handler = headerParser({
+      decorator: 'headers', key: 'accept-language', supportedLngs: [] })
+    let req = { detectedLng: 'en',
+      headers: { 'accept-language': 'en-GB;q=0.9,hu;q=0.7' } }
+    const expected = [ { code: 'en', region: 'GB', quality: 0.9, script: null },
+      { code: 'hu', region: null, quality: 0.7, script: null }]
+    handler(req, {}, function () {
+      t.deepEqual(req.detectedLng, expected,
+        'parses array of language objects found in header'
+      )
+    })
+  })
 })
 
 test('Integration', t => {
@@ -125,6 +218,12 @@ function testRegisterError (t, opts, check, msg) {
       t.ok(err instanceof Error)
       t.match(err.message, check, msg)
     })
+}
+
+function testParser (t, handler, req, ctx, check, msg) {
+  handler(req, {}, function () {
+    t.equal(req.detectedLng, check, `${ctx} | ${msg}`)
+  })
 }
 
 function testRoute (t, instance, url, headers, check, msg) {
